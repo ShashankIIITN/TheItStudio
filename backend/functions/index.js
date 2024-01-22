b342d0e276a282620e47eab11b82450e6558bb5e
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2024-01-23 02:00:59 +0530

    added toml

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import postRoutes from './routes/apis.js';
 import dotenv from "dotenv";
+import serverless from "serverless-http"
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 
-app.use('/', postRoutes);
+app.use('/.netlify/functions/', postRoutes);
 const CONNECTION_URI = process.env.CONNECTION_URI || 'mongodb+srv://Shashank:RQ9Pj8W.rmkb3!$@saitma.hqkmz6j.mongodb.net/TheItStudio?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
@@ -24,5 +25,5 @@ mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: tr
     .then(app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-// mongoose.set('useFindAndModify', false);
+serverless(app);
 
